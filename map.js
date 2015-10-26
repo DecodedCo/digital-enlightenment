f15d5138bc55887b796bc680974a274fb7b0fca1
Amadeus Stevenson <amadeus@decoded.us>
2015-10-26 15:44:21 -0400

    Add Google Maps Traffic Layer

---
+++ map.js
@@ -0,0 +1,23 @@
+/*
+
+  Plot a Google Map with Live Traffic
+
+  Requires in the <head>:
+
+  <script src="https://maps.googleapis.com/maps/api/js?key=API_KEY">
+
+  Requires this file referenced in the bottom of <body>:
+
+  <script src="map.js"></script>
+
+  Requires <div id="map"></div> where you want to place the map
+
+*/
+
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 13,
+  center: {lat: 34.04924594193164, lng: -118.24104309082031}
+});
+
+var trafficLayer = new google.maps.TrafficLayer();
+trafficLayer.setMap(map);
