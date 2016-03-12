1a8065c6e68c0d005da6c487bfa44232ab174153
Amadeus Stevenson <amadeus@decoded.us>
2016-03-12 14:42:24 -0500

    Websockets version

--- dashboard.js
+++ dashboard.js
@@ -67,73 +67,71 @@ var line = d3.svg.line().interpolate("monotone")
   .x(function(d){ return xScale(d.x); })
   .y(function(d){ return yScale(d.y); });
 
-render();
-
-// continuous page render
-setInterval(render, 500);
-
 // bring in sensor data and render graph
-function render(){
+function render(data){
 
-  $.ajax({
-    url: "https://labs.decoded.com/tech/sensor-api/" + sensor + "/",
-    //url: "http://127.0.0.1:5000/" + sensor,
-    type: "GET",
-    success: function(d){
-      var data = d;
-      var newData = [[]];
+  var newData = [[]];
 
-      for(i=0; i<data.length; i++){
-        xpos = data[i].x/100;
-        ypos = data[i].y;
-        newData[0].push({x:xpos, y:ypos});
-      }
+  for(i=0; i<data.length; i++){
+    xpos = data[i].x/100;
+    ypos = data[i].y;
+    newData[0].push({x:xpos, y:ypos});
+  }
 
-      // obtain absolute min and max
-      var yMin = newData.reduce(function(pv,cv){
-        var currentMin = cv.reduce(function(pv,cv){
-          return Math.min(pv,cv.y);
-        },0)
-        return Math.min(pv,currentMin);
-      },0);
-      var yMax = 100;
+  // obtain absolute min and max
+  var yMin = newData.reduce(function(pv,cv){
+    var currentMin = cv.reduce(function(pv,cv){
+      return Math.min(pv,cv.y);
+    },0)
+    return Math.min(pv,currentMin);
+  },0);
+  var yMax = 100;
 
-      // set domain for axis
-      yScale.domain([yMin,yMax]);
+  // set domain for axis
+  yScale.domain([yMin,yMax]);
 
-      // create axis scale
-      var yAxis = d3.svg.axis()
-          .scale(yScale).orient("left");
+  // create axis scale
+  var yAxis = d3.svg.axis()
+      .scale(yScale).orient("left");
 
-      // if no axis exists, create one, otherwise update it
-      if (svg.selectAll(".y.axis")[0].length < 1 ){
-        svg.append("g")
-            .attr("class","y axis")
-            .call(yAxis);
-      } else {
-        svg.selectAll(".y.axis").transition().duration(200).call(yAxis);
-      }
+  // if no axis exists, create one, otherwise update it
+  if (svg.selectAll(".y.axis")[0].length < 1 ){
+    svg.append("g")
+        .attr("class","y axis")
+        .call(yAxis);
+  } else {
+    svg.selectAll(".y.axis").transition().duration(200).call(yAxis);
+  }
 
-      // generate line paths
-      var lines = svg.selectAll(".line").data(newData).attr("class","line");
+  // generate line paths
+  var lines = svg.selectAll(".line").data(newData).attr("class","line");
 
-      // transition from previous paths to new paths
-      lines.transition().duration(10)
-        .attr("d",line)
-        .style("stroke", "black");
+  // transition from previous paths to new paths
+  lines.transition().duration(10)
+    .attr("d",line)
+    .style("stroke", "black");
 
-      // enter any new data
-      lines.enter()
-        .append("path")
-        .attr("class","line")
-        .attr("d",line)
-        .style("stroke", "black");
+  // enter any new data
+  lines.enter()
+    .append("path")
+    .attr("class","line")
+    .attr("d",line)
+    .style("stroke", "black");
 
-      // exit
-      lines.exit()
-        .remove();
-
-      } // end of ajax success function
-    }); // end of ajax
+  // exit
+  lines.exit()
+    .remove();
 
 } // end of render
+
+/*
+
+  Live Sensor Data via websockets
+
+*/
+
+//var socket = io('127.0.0.1:5000/' + sensor);
+var socket = io('https://labs.decoded.com/tech/sensor-api/' + sensor);
+socket.on('data', function(data){
+  render(data);
+});
