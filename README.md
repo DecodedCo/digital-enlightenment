6180ff0425b5b27c53abc23143f7b38f2d7b1b0b
sophieheb <sophie@Decoded-Sophie.local>
2016-03-31 15:44:06 -0400

    Added logic to countdown.js

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ Demystifying Digital Boilerplate
 
 This boilerplate will help you get your reward apps going.
 
-# 1. Realtime Countdown
+# 1. HTML & CSS build
+
+## helper.html
+
+Boilerplate to optimise page for mobile web app experience.
+
+# 2. Realtime Countdown
 
 ## countdown.js
 
 Boilerplate to add realtime countdown. Requires both jQuery and the jQuery.countdown plugin.
 
-## helper.html
-
-Boilerplate to optimise page for mobile web app experience.
--- countdown.js
+++ countdown.js
@@ -10,6 +10,21 @@ var minutes = 30;
 var rewardDate = new Date(year, month-1, day, hours, minutes);
 
 // Add live countdown to the element with id "countdown"
-jQuery("#countdown").countdown(year + "/" + month + "/" + day+ " " + hours + ":" + minutes +":00", function(event) {
-    jQuery(this).text(event.strftime('Only %-D day%!D %H:%M:%S until your destination is revealed.'));
-});
+jQuery("p#countdown").countdown(year + "/" + month + "/" + day+ " " + hours + ":" + minutes +":00", 
+	function(event) {
+	    jQuery(this).text(event.strftime('Only %-D day%!D %H:%M:%S until your destination is revealed.'));
+	});
+
+/*
+
+// Logic
+var timeNow = Date.now();
+var timeReward = rewardDate.getTime();
+
+// If it is time
+if ( ) {
+	jQuery("section").append("<p>You are going to Secret Location.</p>");
+	jQuery("p#countdown").hide();
+}
+
+*/
