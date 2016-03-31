dab759b324353950020963f7415fefe5f5a3d87d
sophieheb <sophie@Decoded-Sophie.local>
2016-03-31 16:19:32 -0400

    Removed reference to my recipe name and added in a generic reference

--- trigger.js
+++ trigger.js
@@ -4,5 +4,5 @@ jQuery("section").append("<button>We're here!</button>");
 // When your button is clicked, do your ifttt recipe.
 $("button").click(function() {
        $.get("https://maker.ifttt.com/trigger/Hue/with/key/c61OpeRCP2Akr6yD9YqSF2");
-           $.get("https://maker.ifttt.com/trigger/sophie_test_4/with/key/c61OpeRCP2Akr6yD9YqSF2");
+           $.get("https://maker.ifttt.com/trigger/RECIPE NAME/with/key/c61OpeRCP2Akr6yD9YqSF2");
 });
