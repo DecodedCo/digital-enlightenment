28046c264ad0ea563cbe337b51452d600e07dca3
Amadeus <github@amadeus.me.uk>
2016-04-01 15:22:32 -0400

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -1,8 +1,8 @@
-// Add a button to your section
-jQuery('section').append('<p id="ifttt"><button>We\'re here!</button></p>');
-
-// When your button is clicked, activate your ifttt recipe
-// Replace TRIGGERNAME with your trigger
-$('button').click(function() {
-  $.get('https://maker.ifttt.com/trigger/TRIGGERNAME/with/key/O0h3mC9aUQQ8wkXhenwDr');
-});
+  // Add a button to your section
+  jQuery('section').append('<p id="ifttt"><button>We\'re here!</button></p>');
+  
+  // When your button is clicked, activate your ifttt recipe
+  // Replace TRIGGERNAME with your trigger
+  $('button').click(function() {
+    $.get('https://maker.ifttt.com/trigger/TRIGGERNAME/with/key/O0h3mC9aUQQ8wkXhenwDr');
+  });
