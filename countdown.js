698a356d1009b5924a8289e5ddf30a63c727024d
Amadeus <github@amadeus.me.uk>
2016-04-01 13:54:11 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,21 +1,20 @@
-// Set the date the reward is revealed
-// Example below is for 4/1/16 at 6:30pm
+// Set the reward date and time
 var year = 2016;
 var month = 04;
 var day = 05;
 var hours = 18;
 var minutes = 30;
+// Example above is for April 5th, 2016 at 6:30pm
 
 // Convert into a JavaScript Date object
 var rewardDate = new Date(year, month-1, day, hours, minutes);
 
-// Add live countdown to the element with id 'countdown'
+// Use jQuery's countdown plugin to add live counter to the p element with id 'countdown'
 jQuery('p#countdown').countdown(year + '/' + month + '/' + day+ ' ' + hours + ':' + minutes +':00',
   function(event) {
-  jQuery(this).text(event.strftime('Only %-D day%!D %H:%M:%S until your destination is revealed.'));
-});
-
-/*
+    jQuery(this).text(event.strftime('Only %-D day%!D %H:%M:%S until your destination is revealed.'));
+  }
+);
 
 // Logic
 var timeNow = Date.now();
@@ -23,8 +22,8 @@ var timeReward = rewardDate.getTime();
 
 // If it is time
 if ( ) {
-  jQuery('section').append('<p>You are going to Secret Location.</p>');
+  // Display the location in the app, at the end of the section
+  jQuery('section').append('<p>You are going to Location Name!</p>');
+  // Hide the live countdown
   jQuery('p#countdown').hide();
 } //End of if statement
-
-*/
