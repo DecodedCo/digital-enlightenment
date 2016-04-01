600a012a22f40c9823e12157a53210b4758f9167
Amadeus <github@amadeus.me.uk>
2016-04-01 15:40:27 -0400

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -4,5 +4,8 @@
   // When your button is clicked, activate your ifttt recipe
   // Replace TRIGGERNAME with your trigger
   $('button').click(function() {
+    // Trigger the IFFFT event
     $.get('https://maker.ifttt.com/trigger/TRIGGERNAME/with/key/O0h3mC9aUQQ8wkXhenwDr');
+    // UX: fade the button in and out
+		$('button').fadeOut("slow").fadeIn("slow");
   });
